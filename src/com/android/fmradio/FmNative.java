8ae8735ced3f0ff3669b32dba79e323e6d4cfdbe
Ash Blake <ash.blake@tutanota.com>
2021-07-07 08:30:30 +0000

    FmNative: remove desense setter functions
    Signed-off-by: Aryan Sinha <sinha.aryan03@gmail.com>

--- src/com/android/fmradio/FmNative.java
+++ src/com/android/fmradio/FmNative.java
@@ -161,8 +161,6 @@ public class FmNative {
     static native short[] emcmd(short[] s);
     static native boolean emsetth(int a, int b);
     static native int[] getHardwareVersion();
-    static native boolean setDesenseChannel(float f, int a);
-    static native boolean setDesenseList(int a, float f);
     static native boolean setChParam(float a, float b);
     static native boolean setSeekSpace(int a);
 }
